d35b11ec25b34ca3779d6e917ad65931d7f3bfd5
Steven Anderson <steven@sjanderson.org>
2018-10-09 16:54:40 -0700

    update legacy secret generator
    Signed-off-by: Steven Anderson <steven@sjanderson.org>

--- src/server/utils/decodeLegacySecretGenerator.mjs
+++ src/server/utils/decodeLegacySecretGenerator.mjs
@@ -1,7 +1,14 @@
 import { decode } from 'jwt-simple';
+import getShard from './getShard';
 
 export default firebaseShards => ({ shard, token }) => {
-  if (!firebaseShards[shard]) {
+  let secret;
+  for (const { databaseURL, legacySecret } of firebaseShards) {
+    if (getShard(databaseURL) === shard) {
+      secret = legacySecret;
+    }
+  }
+  if (!secret) {
     throw new Error(`invalid shard: ${shard}`);
   }
   const { legacySecret } = firebaseShards[shard];
